2590eb1c5801a7319d261a7f59671f7a009a9d4a
luoju <QWE123qwe!>
2024-06-04 13:01:32 +0800

    还原

--- hook.js
+++ hook.js
@@ -2,14 +2,24 @@
 //to hook a function and change its behavior
 //in this case we hook the function that is responsible for the decryption of the license
 //and we change the behavior to return a valid license
-
 // JUST FOR LEARNING PURPOSES, DON'T USE THIS TO CRACK SOFTWARE
-
 //Adding hook
 const crypto = require("crypto");
 const pubdec = crypto["publicDecrypt"];
 delete crypto["publicDecrypt"];
 let fingerprint, email, uuid, license, computerInfo = "";
+let License = ""
+crypto.publicDecrypt = function (key, buffer) {
+    log("PubDec Key:" + key);
+    log("buf: " + buffer.toString('base64'));
+    if (buffer.slice(0, 26).compare(Buffer.from("CRACKED_BY_DIAMOND_HUNTERS")) == 0) {
+        License = buffer.toString('base64');
+        let ret = buffer.toString().replace("CRACKED_BY_DIAMOND_HUNTERS", "");
+        log("backdoor data,return : " + ret);
+        return Buffer.from(ret);
+    }
+    return pubdec(key, buffer);
+};
 
 const fetch = require("electron-fetch")
 fetch_bak = fetch['default'];
@@ -30,6 +40,14 @@ fetch.default = async function fetch(url, options) {
         ret = await data.buffer();
         log('[fetch]Ret ' + ret.toString());
 
+        ret = Buffer.from('{"code":0,"retry":true,"msg":"' + Buffer.from("CRACKED_BY_DIAMOND_HUNTERS" + JSON.stringify(
+            {
+                "fingerprint": fingerprint,
+                "email": email,
+                "license": license,
+                "type": ""
+            })).toString('base64') + '"}');
+        log("replace ret: " + ret.toString());
         data.text = () => {
             return new Promise((resolve, reject) => {
                 resolve(ret.toString());
@@ -41,29 +59,39 @@ fetch.default = async function fetch(url, options) {
             });
         };
     }
-    
+    if (url.indexOf('api/client/renew') != -1) {
+        ret = await data.buffer();
+        log('[fetch]Ret ' + ret.toString());
+
+        ret = Buffer.from('{"success":true,"code":0,"retry":true,"msg":"' + License + '"}');
+        log("replace ret: " + ret.toString());
+        data.text = () => {
+            return new Promise((resolve, reject) => {
+                resolve(ret.toString());
+            });
+        };
+        data.json = () => {
+            return new Promise((resolve, reject) => {
+                resolve(JSON.parse(ret.toString()));
+            });
+        };
+    }
+
     return new Promise((resolve, reject) => {
         resolve(data);
     });
-
 }
-
 http = require("http")
-
 function log(str) {
     http.get('http://127.0.0.1:3000/log?str=' + str, res => {
     }).on('error', err => {
         console.log('Error: ', err.message);
     });
 }
-
 log = console.log;
 log('Hook Init')
-
-
 var Module = require('module');
 var originalRequire = Module.prototype.require;
-
 Module.prototype.require = function () {
     log('Require ' + arguments[0])
     if (arguments[0] == 'crypto') {
@@ -76,8 +104,6 @@ Module.prototype.require = function () {
     }
     return originalRequire.apply(this, arguments);
 };
-
-
 console.log = log
 let validator = {
     set: function (target, key, value) {
@@ -88,9 +114,7 @@ let validator = {
         target[key] = value;
     }
 }
-
 let proxy = new Proxy(console, validator);
 console = proxy
 module.exports = fetch
-
 //hook finished
